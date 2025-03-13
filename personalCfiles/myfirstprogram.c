6ad52de6c5bd7d0522ae308bf073ccdee4e15efd
AldBurr <atb7084@rit.edu>
2025-03-13 15:38:04 -0400

    Add files via upload

---
+++ personalCfiles/myfirstprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNum = 15;
+    int myOtherNum = 26;
+    int sum = myNum + myOtherNum;
+    int x, y, z;
+    x = y = z = 50;
+    printf("%d ", sum);
+    printf("%d %d %d", x, y, z);
+    return 0;
+}
